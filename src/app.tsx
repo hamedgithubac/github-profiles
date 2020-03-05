43017a5a7fff435fcdc1d655bd4f96016bd0f3c5
Hamed Hafezi <h.hafezi93@gmail.com>
2020-03-06 01:47:56 +0330

    Initial Commit/Webpack Configuration

---
+++ src/app.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './a.sass';
+const App = () => {
+    return(
+        <div>Initial Commit in GitHubProfiles</div>
+    )
+}
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"]
+  },
+  mode: "development",
+  entry: path.resolve(__dirname, "src", "index.tsx"),
+  devServer: {
+    contentBase: path.join(__dirname, "public"),
+    compress: true,
+    port: 3002
+  },
+  module: {
+    rules: [
+      { test: /\.(js)$/, use: "babel-loader", exclude: /node_modules/ },
+      { test: /\.ts(x?)$/, use: "ts-loader", exclude: /node_modules/ },
+      {
+        test: /\.s[ac]ss$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      },
+      { test: /\.(png|svg|jpg|gif)$/i, use: "file-loader" },
+      {
+        enforce: "pre",
+        test: /\.js$/,
+        loader: "source-map-loader"
+      }
+    ]
+  },
+//   plugins: [
+//     new HtmlWebpackPlugin({
+//       template: "./src/index.html"
+//     })
+//   ],
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "index_bundle.js"
+  }
+};
